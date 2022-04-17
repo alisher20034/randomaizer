9fe0ea5614d53ef158964b0e50e0fd425e23a399
alisher20034 <jakshilikov1515@gmail.com>
2022-04-18 03:13:47 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let cards_block = document.querySelector('.cards');
+let cards = document.querySelectorAll('.cards > div');
+
+function start() {
+  let random = Math.floor(Math.random() * 9); // От 0 до 8
+  cards_block.style.left = -random * 100 + 'px';
+  setTimeout(function() {
+    random++;
+    cards[random].style.background = '#7B90F7';
+    cards[random].style.color = 'white';
+    
+  }, 5000)
+}
